cb577070cce5595608593b00cf460027fe1f1a89
Toon Bosmans <toonbosmans@Toon.local>
2020-10-24 10:50:40 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -5,25 +5,95 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
+  //variable for url
+  const url = "http://localhost:4500";
+  //create the state to hold the dogs
+  const [dogs, setDogs] = React.useState([]);
+  //empty dog
+  const emptyDog = {
+    name: "",
+    age: 0,
+    img: "",
+  };
+  //select a dog
+  const [selectedDog, setSelectedDog] = React.useState(emptyDog);
+
+  //make function that calls API to get the dogs
+  const getDogs = () => {
+    fetch(url + "/dog/")
+      .then((response) => response.json())
+      .then((data) => setDogs(data));
+  };
+
+  //useEffect to do initial fetch of dogs
+  React.useEffect(() => getDogs(), []);
+
+  //handleCreate function for creating new dogs
+  const handleCreate = (newDog) => {
+    fetch(url + "/dog/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDog),
+    }).then(() => {
+      getDogs();
+    });
+  };
+
+  const handleUpdate = (dog) => {
+    fetch(url + "/dog/" + dog._id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dog),
+    }).then(() => {
+      // don't need the response from the post but will be using the .then to update the list of dogs
+      getDogs();
+    });
+  };
+
+  const selectDog = (dog) => {
+    setSelectedDog(dog);
+  };
+
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
       <main>
+        <Link to="/create">
+          <button>Add Dog</button>
+        </Link>
         <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+          <Route
+            exact
+            path="/"
+            render={(rp) => <Display {...rp} dogs={dogs} />}
+          />
           <Route
             exact
             path="/create"
             render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+              <Form
+                {...rp}
+                label="create"
+                dog={emptyDog}
+                handleSubmit={handleCreate}
+              />
             )}
           />
           <Route
             exact
             path="/edit"
             render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+              <Form
+                {...rp}
+                label="update"
+                dog={{ selectedDog }}
+                handleSubmit={handleUpdate}
+              />
             )}
           />
         </Switch>
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  const { dogs } = props;
+
+  const loaded = () => {
+    return (
+      <div style={{ textAlign: "center" }}>
+        {dogs.map((dog) => (
+          <article>
+            <img src={dog.img} style={{ maxWidth: "400px" }} />
+            <h1>{dog.name}</h1>
+            <h3>{dog.age}</h3>
+          </article>
+        ))}
+      </div>
+    );
+  };
+
+  const loading = <h1>Loading...</h1>;
+
+  return dogs.length > 0 ? loaded() : loading;
 };
 
 export default Display;
